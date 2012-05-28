04ad88386773222a3d96aa564b14ad1a52ad49f5
Fernando Alonso <krakatoa1987@gmail.com>
2012-05-27 21:41:07 -0300

    Updated Bliss::Constraint

--- lib/bliss/constraint.rb
+++ lib/bliss/constraint.rb
@@ -1,72 +1,78 @@
 module Bliss
   class Constraint
-    attr_reader :field, :type, :state
+    #attr_reader :field, :type, :state
+    attr_reader :depth, :setting, :state
 
-    TYPES = [:exist, :not_blank, :possible_values]
+    #TYPES = [:exist, :not_blank, :possible_values]
 
-    # TODO use depth on each constraint
-
-    def initialize(field, type, possible_values=nil)
-      if field.is_a? Array
-        @field = field
-      else
-        @field = [field]
-      end
-      @type = type
-      @possible_values = possible_values
+    def initialize(depth, setting, params={})
+      @depth = depth
+      @setting = setting
 
       @state = :not_checked
     end
 
+    #def initialize(field, type, possible_values=nil)
+    #  if field.is_a? Array
+    #    @field = field
+    #  else
+    #    @field = [field]
+    #  end
+    #  @type = type
+    #  @possible_values = possible_values
+    #
+    #  @state = :not_checked
+    #end
+
     def run!(hash)
       @state = :not_checked
-      @field.each do |field|
-        if @state == :passed
-          break
+      #@field.each do |field|
+        #if @state == :passed
+        #  break
+        #end
+        case @setting
+          when :tag_name_required
+            if !hash.keys.include?(depth.last)
+              @state = :not_passed
+            else
+              @state = :passed
+            end
+          #when :not_blank
+          #  if hash.has_key?(field) and !hash[field].to_s.empty?
+          #    @state = :passed
+          #  else
+          #    @state = :not_passed
+          #  end
+          #when :possible_values
+          #  if hash.has_key?(field) and @possible_values.include?(hash[field])
+          #    @state = :passed
+          #  else
+          #    @state = :not_passed
+          #  end
         end
-        case @type
-          when :exist
-            if !hash.keys.include?(field)
-              @state = :not_passed
-            else
-              @state = :passed
-            end
-          when :not_blank
-            if hash.has_key?(field) and !hash[field].to_s.empty?
-              @state = :passed
-            else
-              @state = :not_passed
-            end
-          when :possible_values
-            if hash.has_key?(field) and @possible_values.include?(hash[field])
-              @state = :passed
-            else
-              @state = :not_passed
-            end
-        end
-      end
+      #end
       @state
     end
 
     def detail
       if @state == :not_passed
         detail = case @type
-          when :exist
+          when :tag_name_required
             [@field.join(" or "), "missing"]
-          when :not_blank
-            [@field.join(" or "), "blank"]
-          when :possible_values
-            [@field.join(" or "), "invalid"]
+          #when :not_blank
+          #  [@field.join(" or "), "blank"]
+          #when :possible_values
+          #  [@field.join(" or "), "invalid"]
         end
       end
     end
 
-    def self.build_constraint(field, types, possible_values=nil)
-      constraints = []
-      constraints.push Bliss::Constraint.new(field, :exist) if types.include?(:exist)
-      constraints.push Bliss::Constraint.new(field, :not_blank) if types.include?(:not_blank)
-      constraints.push BlissConstraint.new(field, :possible_values, possible_values) if types.include?(:possible_values)
-      constraints
-    end
+    #def self.build_constraint(depth, setting, params={})#, field, type, possible_values=nil)
+    #  constraints = []
+      #constraints.push Bliss::Constraint.new(field, :exist) if types.include?(:exist)
+      #constraints.push Bliss::Constraint.new(field, :not_blank) if types.include?(:not_blank)
+      #constraints.push BlissConstraint.new(field, :possible_values, possible_values) if types.include?(:possible_values)
+    #  constraints
+    #end
   end
 end
--- lib/bliss/format.rb
+++ lib/bliss/format.rb
@@ -22,6 +22,8 @@ module Bliss
     def constraints
       return [] if not (@specs.is_a? Hash and @specs.size > 0)
 
+      constraints = []
+
       @specs.recurse(true) do |depth, value|
         if !@@keywords.include?(depth.last)
           settings = @specs.value_at_chain(depth).select{|key| @@keywords.include?(key) }
@@ -29,17 +31,32 @@ module Bliss
         if settings
           settings.merge!({"tag_name_required" => true}) if not settings.has_key?("tag_name_required")
 
+          puts settings.inspect
+
           # tag_name_required constraint:
 
-          
+          settings.each_pair { |setting, value|
+            case setting
+              when "tag_name_required"
+                if value == true
+                  constraints.push(Bliss::Constraint.new(depth, :tag_name_required))
+                end
+            end
+          }
+
+          #required_fields.each do |field|
+          #  constraints.concat(Sumavisos::Parsers::Constraint.build_constraint(field, [:exist, :not_blank]).dup)
+          #end
 
           ###
 
           #puts "#{depth.join('/')}: #{settings.inspect}"
         end
       end
+
+      puts constraints.inspect
       
-      constraints = []
+      return constraints
     end
     
     # during parsing
@@ -52,9 +69,9 @@ module Bliss
       #required_fields.concat(Sumavisos::Parsers::Validator::FIELDS[vertical]['required'])
       
       #constraints = []
-      required_fields.each do |field|
-        constraints.concat(Sumavisos::Parsers::Constraint.build_constraint(field, [:exist, :not_blank]).dup)
-      end
+      #required_fields.each do |field|
+      #  constraints.concat(Sumavisos::Parsers::Constraint.build_constraint(field, [:exist, :not_blank]).dup)
+      #end
 
       if vertical == 'property'
         constraints.concat(Sumavisos::Parsers::Constraint.build_constraint(['type'], [:possible_values], Sumavisos::Parsers::Validator::VALID_PROPERTY_TYPES).dup)
