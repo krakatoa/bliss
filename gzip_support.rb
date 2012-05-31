99c15e074259ce3eedd3e650f6da145f54ed6045
Fernando Alonso <krakatoa1987@gmail.com>
2012-05-31 09:46:36 -0300

    Updated gzip_support.rb

--- gzip_support.rb
+++ gzip_support.rb
@@ -10,17 +10,6 @@ require 'zlib'
 f = File.new('test.xml', 'w')
 
 EM.run do
-  # jp
-  #url = 'http://www.workgate.co.jp/feeds/sumavisos/sumavisos.xml'
-  url = 'http://feeds.motoseller.com/feeds/?api_key=868b518ffa41be698cb02c189bbe52173d3d8aed&feed=usa-motosell-trovit'
-
-  # gz
-  #url = 'http://www.vivastreet.co.in/feed/download/generic_cars-cars_trucks-active.xml.gz' #"application/octet-stream"
-  #url = 'http://www.autocosmos.com.ar/webservices/exchange/sumavisos.ar.xml.gz'# 'application/x-gzip'
-  
-  # zip
-  # url = 'http://www.nexolocal.com.br/nl_xml/trovit/br/vehiculos.zip?user=sumavisos&pass=fsdgjfgsjdgf42354723' # 'application/zip'
-
   http = EM::HttpRequest.new(url, :inactivity_timeout => 1).get # :head => {'accept-encoding' => "gzip, deflate"}
   gzipped = false
   http.headers do
